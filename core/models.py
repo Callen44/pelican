890683caa8f5c68a7f7667982e08025fe91df1d4
caleb's dektop <97454255+Callen44@users.noreply.github.com>
2023-02-27 17:48:25 -0600

    Revert "accept current change in main branch"
    This reverts commit adddef20535d4b32440120dc9c5083e292a7b720.

--- core/models.py
+++ core/models.py
@@ -2,23 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    post_title = models.CharField(max_length=200)
-    post_body = models.CharField(max_length=1000)
-    pub_date = models.DateTimeField('date published')
-    by = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.post_title
-    def get_id(self):
-        return self.id
-    def get_body(self):
-        return self.post_body
-    def get_date(self):
-        return self.pub_date
-    def get_name(self):
-        return self.post_title
-    def get_author(self):
-        return self.by
-
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=1000)
+    published_date = models.DateTimeField('date published')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 class Like(models.Model):
     posts = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     users = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
