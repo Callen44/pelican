2b4e4c12195ba5930dc78e7836af1cedeb4d449d
callen44 <caleballen44@outlook.com>
2023-03-04 19:15:56 -0600

    test update view

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import datetime
+import os
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.test import Client
+
+from .views import update
+from .models import Post, Like
+
+
+class postTests(TestCase):
+
+    def test_update_view(self):
+        # activate the client
+        client = Client()
+
+        # make a test user
+        user = User.objects.create_user(username='testuser', password='testpass')
+        client.force_login(user)
+
+        # create the 2 posts
+        p1 = Post(title = "title", body = "blank", published_date = timezone.now(), created_by = user)
+        p1.save()
+        
+        # the seccond post uses the same title
+        p2 = Post(title = "title", body = "blank", published_date = timezone.now(), created_by = user)
+        p2.save()
+
+        # run the actual client
+        response = client.get('/1/update/')
+
+        assert response.status_code == 200
