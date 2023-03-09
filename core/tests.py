2a23c191fae30db2b825c9cc98796f965ac4905c
callen44 <caleballen44@outlook.com>
2023-03-09 17:27:36 -0600

    test delete view

--- core/tests.py
+++ core/tests.py
@@ -52,3 +52,28 @@ class postTests(TestCase):
         # run the actual test
         response = client.get('/')
         assert response.status_code == 200
+
+    def test_delete_view(self):
+        # activate the client
+        client = Client()
+
+        # make a test user
+        user = User.objects.create_user(username='testuser', password='testpass')
+        client.force_login(user)
+
+        # create the 2 posts
+        p1 = Post(title = "title", body = "blank", published_date = timezone.now(), created_by = user)
+        p1.save()
+        
+        # the seccond post uses the same title
+        p2 = Post(title = "title", body = "blank", published_date = timezone.now(), created_by = user)
+        p2.save()
+
+        # save the id of the p2 post
+        p1_id = p1.id
+
+        # run the actual test
+        print('/'+str(p1.id)+'/delete/')
+        response = client.get('/'+str(p1.id)+'/delete/')
+        
+        self.assertFalse(Post.objects.filter(id=p1.id).exists())
