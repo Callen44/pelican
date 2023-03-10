ca6b9fae9169f7be569e49be7bc3f6f850c02dcc
callen44 <caleballen44@outlook.com>
2023-03-10 14:56:12 -0600

    add test for create

--- core/tests.py
+++ core/tests.py
@@ -12,11 +12,13 @@ from .models import Post, Like
 
 class PostTests(TestCase):
     def setUp(self):
+        # creates a user and logs into the client
         self.user = User.objects.create_user(username='testuser', password='testpass')
         self.client = Client()
         self.client.force_login(self.user)
 
     def test_update_view(self):
+        # tests the view that updates posts
         posts = [Post.objects.create(title='title', body='blank', published_date=timezone.now(), created_by=self.user) for _ in range(2)]
         p1 = posts[0]
 
@@ -36,15 +38,35 @@ class PostTests(TestCase):
         self.assertEqual(p1.title, 'I am a mongoose')
 
     def test_index_view(self):
-        posts = [Post.objects.create(title='title', body='blank', published_date=timezone.now(), created_by=self.user) for _ in range(2)]
+        # checks that the index view responds with 200
 
+        # yes I know this test could be better, like
+        # testing things like the info provided that is
+        # shown. but these are my first posts, and I am
+        # not shure how this should be best adapted in
+        # the future
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_delete_view(self):
+        # deletes a post to make sure the view works
         posts = [Post.objects.create(title='title', body='blank', published_date=timezone.now(), created_by=self.user) for _ in range(2)]
         p1 = posts[0]
 
         response = self.client.get(f'/{p1.id}/delete/')
         self.assertEqual(response.status_code, 302)
         self.assertFalse(Post.objects.filter(id=p1.id).exists())
+    def test_create_view(self):
+        # this tests the create view
+        posts = [Post.objects.create(title='title', body='blank', published_date=timezone.now(), created_by=self.user) for _ in range(2)]
+        p1 = posts[0]
+
+        response = self.client.get(f'/{p1.id}/update/')
+        csrf_token = response.cookies['csrftoken'].value
+
+        data = {
+            'title': 'I exist',
+            'body': "If I don't then something is wrong",
+            'csrfmiddlewaretoken': csrf_token,
+        }
+        response = self.client.post('/create', data=data)
