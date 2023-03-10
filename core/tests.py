d0e484bd3c24a4404b247ccae219a0afc4e2dd85
callen44 <caleballen44@outlook.com>
2023-03-10 07:42:21 -0600

    added a test for the update view, and fixed the it

--- core/tests.py
+++ core/tests.py
@@ -14,27 +14,34 @@ from .models import Post, Like
 class postTests(TestCase):
 
     def test_update_view(self):
-        # activate the client
-        client = Client()
-
-        # make a test user
+        # Create a test user and log them in
         user = User.objects.create_user(username='testuser', password='testpass')
+        client = Client()
         client.force_login(user)
 
-        # create the 2 posts
-        p1 = Post(title = "title", body = "blank", published_date = timezone.now(), created_by = user)
-        p1.save()
-        
-        # the seccond post uses the same title
-        p2 = Post(title = "title", body = "blank", published_date = timezone.now(), created_by = user)
-        p2.save()
+        # Create two posts
+        p1 = Post.objects.create(title='title', body='blank', published_date=timezone.now(), created_by=user)
+        p2 = Post.objects.create(title='title', body='blank', published_date=timezone.now(), created_by=user)
 
-        # run the actual test
-        response = client.get('/'+str(p1.id)+'/update/',data={'title':'I am a mongose','body':'it is true'})
-        
-        p1_again = Post.objects.get(id = p1.id)
-        print(p1_again.title)
-        assert p1_again.title == 'I am a mongose'
+        # Get a CSRF token
+        response = client.get(f'/{p1.id}/update/')
+        csrf_token = response.cookies['csrftoken'].value
+
+        # Update p1 with new data
+        data = {
+            'title': 'I am a mongoose',
+            'body': 'it is true',
+            'csrfmiddlewaretoken': csrf_token,
+        }
+        response = client.post(f'/{p1.id}/update/', data=data)
+
+        # Check that the response status code is 302, indicating a redirect
+        assert response.status_code == 302
+
+        #Check that the updated post has the correct title
+        p1.refresh_from_db()
+        assert p1.title == 'I am a mongoose'
+
 
     def test_index_view(self):
         # activate the client
