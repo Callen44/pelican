d24254a0da73cf23852bb262422644f82148527e
callen44 <caleballen44@outlook.com>
2023-03-25 11:25:54 -0500

    remove setup.sql (don't need it)

--- setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE mydb;
-CREATE USER myuser WITH ENCRYPTED PASSWORD 'mypass';
-ALTER ROLE myuser SET client_encoding TO 'utf8';
-ALTER ROLE myuser SET default_transaction_isolation TO 'read committed';
-ALTER ROLE myuser SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
-ALTER USER myuser CREATEDB; 
